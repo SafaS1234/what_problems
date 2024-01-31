ff855a360531f29934d93db491d970ee3455f6b7
SafaS1234 <157080937+SafaS1234@users.noreply.github.com>
2024-01-30 20:13:52 -0600

    Works for the first user input

---
+++ prob.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+#include<sstream>
+
+using namespace std;
+
+//reads lesson and problem numbers
+void read_problems(char& lesson_letter, int& prob_num)
+{
+    cout << "\nEnter the problem set:  ";
+    cin  >>  lesson_letter >> prob_num;
+
+    //stores the lesson letter
+    lesson_letter = static_cast<char>(toupper(lesson_letter));
+
+    return;
+}
+
+//lists out the problems 
+int find_set(int prob_num)
+{
+    int set_nums ;//= figure it out
+    return set_nums;
+}
+
+int main(void)
+{
+    char lesson_letter;
+    int prob_num = 0, lesson;
+
+    read_problems(lesson_letter, prob_num);
+    lesson = find_set(prob_num);
+    cout << "\nDo problems " << prob_num << " of " << lesson_letter 
+         << "." << endl;
+
+    
+}
